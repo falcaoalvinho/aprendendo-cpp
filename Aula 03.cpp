6ec0bedda744764bc73937b4eb9a03e8b8e9c858
Álvaro José <josealvaro3421@gmail.com>
2025-05-22 15:28:52 -0300

    Primeiro commit

---
+++ Aula 03.cpp
@@ -0,0 +1,13 @@
+//Comando 'include' permite utilizar bibliotecas dentro do projeto
+#include <iostream> // 'iostream' : biblioteca para entrada e saída de dados
+
+// 'namespace : indica em quais bibliotecas ele deve buscar 
+// 'std' : determina biblioteca 'standard', ou padrão 
+using namespace std;
+
+int main(){
+    
+    
+    
+    return 0;
+}
